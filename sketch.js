ac432ba5205fad341388c99a5dcd83ac5e5199d4
Peter Springer <peterspringer829@gmail.com>
2017-11-27 21:59:58 -0600

    basic shapes

--- sketch.js
+++ sketch.js
@@ -1,12 +1,42 @@
-function setup() {
-  createCanvas(640, 480);
+function setup(){
+  createCanvas(710, 400, WEBGL);
 }
 
-function draw() {
-  if (mouseIsPressed) {
-    fill(0);
+let r = 228, g = 181, b = 41
+
+function draw(){
+  background(r, g, b);
+
+  translate(-355, 0, 0);
+
+  translate(240, 0, 0);
+
+  push();
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0);
+  fill(180);
+  box(1000, 80, 80);
+  pop();
+
+  translate(240, 0, 0);
+  push();
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  fill(r, g, b)
+  torus(70, 20);
+  pop();
+}
+
+function mouseClicked() {
+  if (r > 350) {
+    r = 228
+    g = 181
+    b = 41
   } else {
-    fill(255);
+    r += 10
+    g += 10
+    b += 10
   }
-  ellipse(mouseX, mouseY, 80, 80);
 }
