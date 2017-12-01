047cedc17c4b63c70852e3ca6a21a5522e01d8d3
Peter Springer <peterspringer829@gmail.com>
2017-11-30 22:20:38 -0600

    bouncing block passes across the top of target block

--- sketch.js
+++ sketch.js
@@ -1,42 +1,30 @@
-function setup(){
-  createCanvas(710, 400, WEBGL);
+let x, goBack
+
+function setup() {
+  createCanvas(720, 400)
+  loop()
+  x = 0
 }
 
-let r = 228, g = 181, b = 41
+function draw() {
+  background(0)
+  if (x >= 0 && x <= 668) {
+    if (goBack <= 664 && goBack > 0) {
+      x = goBack - 4
+      goBack = goBack - 4
+    } else {
+      x = x + 4
+    }
+    if (x === 664) {
+      goBack = x
+    }
+  }
 
-function draw(){
-  background(r, g, b);
-
-  translate(-355, 0, 0);
-
-  translate(240, 0, 0);
-
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0);
-  fill(180);
-  box(1000, 80, 80);
-  pop();
-
-  translate(240, 0, 0);
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  fill(r, g, b)
-  torus(70, 20);
-  pop();
-}
-
-function mouseClicked() {
-  if (r > 350) {
-    r = 228
-    g = 181
-    b = 41
-  } else {
-    r += 10
-    g += 10
-    b += 10
+  rect(360, 200, 55, 55)
+  fill(101, 103, 177)
+  rect(x, 200, 55, 55)
+  fill(180, 65, 79);
+  if (x === 360) {
+    console.log('hit')
   }
 }
